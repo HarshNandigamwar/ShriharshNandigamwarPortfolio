09c82271df741ecb743adf4f03d865d154457811
Shriharsh Nandigamwar <nandigamwarharsh@gmail.com>
2026-01-28 20:16:34 +0530

    delete unwanted files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,15 @@
+import type { Metadata } from "next";
 import "./globals.css";
 import SmoothScroll from "@/components/SmoothScroll";
 import { ThemeProvider } from "next-themes";
 import { Toaster } from "sonner";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+
+export const metadata: Metadata = {
+  title:
+    "Shriharsh Nandigamwar | A Full Stack Developer passionate about crafting seamless digital experiences. Always excited to tackle new challenges where I can create value and grow as a coder. Let's connect if you've got a project that could use my skills",
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-// "use client";
+"use client";
 import HomePage from "./home/page";
 import AboutPage from "./about/page";
 import ExperiencePage from "./experience/page";
@@ -6,8 +6,13 @@ import SkillsPage from "./skills/page";
 import ProjectsPage from "./projects/page";
 import GitHubStatsPage from "./github/page";
 import ContactPage from "./contact/page";
+import { printConsoleSignature } from "@/Utils/printConsoleSignature";
+import { useEffect } from "react";
 
 export default function Home() {
+   useEffect(() => {
+    printConsoleSignature();
+  }, []);
   return (
     <main className="max-w-7xl mx-auto px-6 py-20 ">
       <HomePage />
