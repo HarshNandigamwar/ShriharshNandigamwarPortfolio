f83f0c16341d24e7461670dbef841440da070b29
Shriharsh Nandigamwar <nandigamwarharsh@gmail.com>
2026-01-29 07:10:45 +0530

    github page style done

--- src/app/github/page.tsx
+++ src/app/github/page.tsx
@@ -8,29 +8,54 @@ export default function GitHubStatsPage() {
     light: ["#ebedf0", "#9be9a8", "#40c463", "#30a14e", "#216e39"],
     dark: ["#161b22", "#0e4429", "#006d32", "#26a641", "#39d353"],
   };
+  const gitCard = [
+    {
+      title: "Consistency",
+      description: "Daily Updates",
+    },
+    {
+      title: "Commitment",
+      description: "Real-time Sync",
+    },
+    {
+      title: "Transparency",
+      description: "Public Repos",
+    },
+  ];
 
   return (
     <section className="py-24 mx-auto">
+      <motion.h2
+        initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        className="text-5xl font-bold flex items-center gap-4 mb-14"
+      >
+        <span className="text-4xl font-bold text-brand">GitHub</span>
+        <div className="h-[1px] bg-brand/20 flex-grow" />
+      </motion.h2>
       <motion.div
         initial={{ opacity: 0, y: 50 }}
         whileInView={{ opacity: 1, y: 0 }}
         viewport={{ once: true }}
-        className="bg-brand/10 border border-brand/30 rounded-2xl p-4 md:p-12 relative overflow-hidden"
+        className="bg-gradient-to-br from-emerald-500/20 to-transparent backdrop-blur-sm border border-brand/30 rounded-2xl p-4 md:p-12 relative overflow-hidden"
       >
-        {/* Background Glow */}
-        <div className="absolute top-0 right-0 -z-10 w-64 h-64 bg-brand/10 rounded-full blur-[100px]" />
-
         <div className="flex flex-col md:flex-row justify-between items-start md:items-center gap-6 mb-10">
           <div>
-            <h2 className="text-3xl font-bold flex items-center gap-3">
-              <Github className="text-brand" size={32} />
+            <h2 className="text-3xl font-bold flex items-center gap-3 justify-center md:justify-start ">
+              <Github className="hidden md:block text-brand" size={32} />
               GitHub Activity
             </h2>
             <p className="text-center md:text-start mt-2">
               My daily contributions and coding consistency on GitHub.
             </p>
           </div>
-          <div className="flex items-center justify-center md:justify-end w-full">
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.3, delay: 0.1 }}
+            whileHover={{ scale: 1.05 }}
+            className="flex items-center justify-center md:justify-end w-full"
+          >
             <a
               href="https://github.com/HarshNandigamwar"
               target="_blank"
@@ -38,7 +63,7 @@ export default function GitHubStatsPage() {
             >
               Visit Profile
             </a>
-          </div>
+          </motion.div>
         </div>
 
         {/* The GitHub Chart */}
@@ -52,23 +77,23 @@ export default function GitHubStatsPage() {
           />
         </div>
 
+        {/* Git Cards */}
         <div className="mt-8 grid grid-cols-1 md:grid-cols-3 gap-4 text-center">
-          <div className="p-4 rounded-2xl bg-white/5 border border-white/5">
-            <p className="text-brand font-bold text-xl uppercase">
-              Consistency
-            </p>
-            <p className="text-xs text-white/40">Daily Updates</p>
-          </div>
-          <div className="p-4 rounded-2xl bg-white/5 border border-white/5">
-            <p className="text-brand font-bold text-xl uppercase">Commitment</p>
-            <p className="text-xs text-white/40">Real-time Sync</p>
-          </div>
-          <div className="p-4 rounded-2xl bg-white/5 border border-white/5">
-            <p className="text-brand font-bold text-xl uppercase">
-              Transparency
-            </p>
-            <p className="text-xs text-white/40">Public Repos</p>
-          </div>
+          {gitCard.map((data, idx) => (
+            <motion.div
+              key={idx}
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.3, delay: idx * 0.1 }}
+              whileHover={{ scale: 1.05 }}
+              className="p-4 rounded-2xl bg-gradient-to-br from-emerald-500/20 to-transparent backdrop-blur-sm border border-brand/30"
+            >
+              <p className="text-brand font-bold text-xl uppercase">
+                {data.title}
+              </p>
+              <p className="text-xs text-white/40">{data.description}</p>
+            </motion.div>
+          ))}
         </div>
       </motion.div>
     </section>
