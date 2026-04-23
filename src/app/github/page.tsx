f85c43375f2f7726261ce14d88b16e6f8f37eac2
Shriharsh Nandigamwar <nandigamwarharsh@gmail.com>
2026-04-23 16:47:40 +0530

    Add real values in github

--- src/app/github/page.tsx
+++ src/app/github/page.tsx
@@ -5,29 +5,36 @@ import {Github} from "lucide-react";
 import {useState, useEffect} from "react";
 import ThinkingDots from "@/components/ThinkingDots";
 
+interface GitHubData {
+    public_repos: number;
+    followers: number;
+    following: number;
+}
 export default function GitHubStatsPage() {
+    const [loading, setLoading] = useState(true);
+    const [githubData, setGitHubData] = useState<GitHubData | null>(null);
     const greenTheme = {
         light: ["#ebedf0", "#9be9a8", "#40c463", "#30a14e", "#216e39"],
         dark: ["#161b22", "#0e4429", "#006d32", "#26a641", "#39d353"],
     };
-    const gitCard = [
-        {
-            title: "Consistency",
-            description: "Daily Updates",
-        },
-        {
-            title: "Commitment",
-            description: "Real-time Sync",
-        },
-        {
-            title: "Transparency",
-            description: "Public Repos",
-        },
-    ];
-    const [isLoading, setIsLoading] = useState(true);
+
     useEffect(() => {
-        const timer = setTimeout(() => setIsLoading(false), 2500);
-        return () => clearTimeout(timer);
+        async function fetchUser() {
+            try {
+                setLoading(true);
+                const [response] = await Promise.all([fetch(`https://api.github.com/users/HarshNandigamwar`)]);
+                if (!response.ok) {
+                    throw new Error(`Error: ${response.status}`);
+                }
+                setGitHubData(await response.json());
+            } catch (error) {
+                setLoading(false);
+                console.log(error);
+            } finally {
+                setLoading(false);
+            }
+        }
+        fetchUser();
     }, []);
 
     return (
@@ -63,7 +70,7 @@ export default function GitHubStatsPage() {
 
                 {/* GitHub Chart */}
                 <div className="flex justify-center overflow-x-auto p-4 bg-black/5 rounded-xl border border-brand/30">
-                    {isLoading ? (
+                    {loading ? (
                         <div className="flex flex-col items-center gap-4 w-full">
                             <div className="flex gap-1 w-full justify-center animate-pulse">
                                 {[...Array(5)].map((_, i) => (
@@ -82,18 +89,37 @@ export default function GitHubStatsPage() {
                 </div>
 
                 {/* Git Cards */}
-                <div className="mt-8 grid grid-cols-1 md:grid-cols-3 gap-4 text-center">
-                    {gitCard.map((data, idx) => (
+                <div className={` mt-8 grid grid-cols-1 md:grid-cols-3 gap-4 mb-8`}>
+                    {[
+                        {
+                            label: "Repositories",
+                            value: githubData?.public_repos || "N/A",
+                            sub: "public projects",
+                        },
+                        {
+                            label: "Followers",
+                            value: githubData?.followers || "N/A",
+                            sub: "web-heads",
+                        },
+                        {
+                            label: "Following",
+                            value: githubData?.following || "N/A",
+                            sub: "in the network",
+                        },
+                    ].map((s, idx) => (
                         <motion.div
-                            key={idx}
+                            key={s.label}
                             initial={{opacity: 0, y: 20}}
                             whileInView={{opacity: 1, y: 0}}
                             transition={{duration: 0.3, delay: idx * 0.1}}
                             whileHover={{scale: 1.05}}
-                            className="p-4 rounded-2xl bg-gradient-to-br from-emerald-500/20 to-transparent backdrop-blur-sm border border-brand/30"
+                            className="p-4 rounded-2xl bg-gradient-to-br from-emerald-500/20 to-transparent backdrop-blur-sm border border-brand/30 text-center"
                         >
-                            <p className="text-brand font-bold text-xl uppercase">{data.title}</p>
-                            <p className="text-xs text-white/40">{data.description}</p>
+                            <div>
+                                <p className="text-brand font-bold text-xl uppercase">{s.label}</p>
+                                <p className="m-1">{loading ? <ThinkingDots /> : <p>{s.value}</p>}</p>
+                                <p className="text-xs text-white/40">{s.sub}</p>
+                            </div>
                         </motion.div>
                     ))}
                 </div>
